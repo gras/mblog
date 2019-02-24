d9116e6a01496d407858b06dc8f02bc3f61740fc
gras <github@gras.org>
2019-02-23 20:19:03 -0500

    working first draft

--- app/models.py
+++ app/models.py
@@ -1,21 +1,34 @@
 from datetime import datetime
 from app import db
 
+class Worklog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    workdate = db.Column(db.DateTime, index=True)
+    hours = db.Column(db.Float)
+    message = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'))
+
+    def __repr__(self):
+        return '<Worklog {} {}>'.format(self.message, self.hours)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    userid = db.Column(db.String(64), index=True, unique=True)
+    fullname = db.Column(db.String(120))
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    u_worklogs = db.relationship('Worklog', backref='u_log', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {} {}>'.format(self.id, self.userid)
 
-class Post(db.Model):
+class Ticket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    ticket = db.Column(db.String(10), index=True, unique=True)
+    sponsor = db.Column(db.String(10))
+    status = db.Column(db.String(15))
+    title = db.Column(db.String(100))
+    t_worklogs = db.relationship('Worklog', backref='t_log', lazy='dynamic')
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return '<Ticket {} {}>'.format(self.sponsor, self.ticket)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app, db
 from app.forms import LoginForm
+from app.models import Worklog, User, Ticket
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -26,3 +28,56 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/load')
+def data_load():
+    
+    u = User(userid='grasmed',fullname='Jon Gras')
+    db.session.add(u)
+    
+    t = Ticket(ticket='REQ-1234',sponsor='OAA',status='In Process',
+               title='Do this work for me')
+    db.session.add(t)
+
+    w = Worklog(workdate=datetime.now(), hours= 2.5, message='did work',
+                u_log=u, t_log=t)
+    db.session.add(w)
+
+    db.session.commit()
+    return 'Loaded'
+
+@app.route('/view')
+def data_view():
+    result = ''
+    
+    users = User.query.all()
+    for u in users:
+        result += str(u.id) +' '+ u.userid +' '
+        
+    tickets = Ticket.query.all()
+    for t in tickets:
+        result += t.sponsor +' '+ t.ticket +' '
+        
+    worklogs = Worklog.query.all()
+    for w in worklogs:
+        result += w.message +' '+ str(w.hours ) +' '+ w.u_log.userid +" "+ w.t_log.sponsor
+    
+    return result
+
+@app.route('/delete')
+def data_delete():
+    users = User.query.all()
+    for u in users:
+        db.session.delete(u)
+        
+    tickets = Ticket.query.all()
+    for t in tickets:
+        db.session.delete(t)
+        
+    worklogs = Worklog.query.all()
+    for w in worklogs:
+        db.session.delete(w)
+        
+    db.session.commit()
+    
+    return 'Deleted'
